d1e0990bb1ba291d60afdf610c5498b7bcbc16cf
sakno <sakno@tncor.com>
2011-09-16 17:37:07 +1100

    Package compilation bug fixed

--- demo1/make_sample.sh
+++ demo1/make_sample.sh
@@ -1,20 +1,20 @@
 # MONO envar - path to the installed Mono
 # TNAPS envar - path to the installed TNAPS
 
-#Building binaries
+echo Building binaries
 $MONO/bin/xbuild ./server/components.sln /p:Configuration=Release /p:Platform="Any CPU"
 
-#Build component packages
+echo Component packages
 
 $MONO/bin/mono $TNAPS/tnpack.exe calculator.tnpak -component -impl ./server/calc/bin/Release/TN.ApplicationServer.Samples.Calculator.dll -author $USER -name Calculator -hpage http://tnaps.tncor.com copy -ver 1.0.0.0 -desc ./server/calc/compdoc.xml ./server/calc/comp.png -type clr -sign:rsa ./server/sign_key.rsa
 
 $MONO/bin/mono $TNAPS/tnpack.exe currency.tnpak -component -impl ./server/currency/bin/Release/TN.ApplicationServer.Samples.Currency.dll -author $USER -name Currency -hpage http://tnaps.tncor.com copy -ver 1.0.0.0 -desc ./server/currency/compdoc.xml ./server/currency/comp.png -type clr -sign:rsa ./server/sign_key.rsa
 
-#Build application package
+echo Application package
 
-$MONO/bin/mono $TNAPS/tnpack.exe demo_app.tnpak -app -author TN -name DemoApplication1 -hpage http://tnaps.tncor.com copy -ver 1.0 -desc ./server/appdoc.xml NONE -dep Name=hostprovider,Version=1.0,SignatureToken=0024000004800000 r hp NONE -dep Name=calculator,Version=1.0,SignatureToken=7F33AE53726B526A d calc NONE -dep Name=currency,Version=1.0,SignatureToken=7F33AE53726B526A d currency NONE -rels hp calc -rels hp currency -sign:rsa ./server/sign_key.rsa -include Calculator.tnpak Calculator.tnpak public -include Currency.tnpak Currency.tnpak public -props ./server/app_props.xml -include:folder client /vdir permanent
+$MONO/bin/mono $TNAPS/tnpack.exe demo_app.tnpak -app -author TN -name DemoApplication1 -hpage http://tnaps.tncor.com copy -ver 1.0 -desc ./server/appdoc.xml ./server/app.png -dep Name=hostprovider,Version=1.0,SignatureToken=0024000004800000 r hp NONE -dep Name=calculator,Version=1.0,SignatureToken=7F33AE53726B526A d calc NONE -dep Name=currency,Version=1.0,SignatureToken=7F33AE53726B526A d currency NONE -rels hp calc -rels hp currency -sign:rsa ./server/sign_key.rsa -include Calculator.tnpak Calculator.tnpak public -include Currency.tnpak Currency.tnpak public -props ./server/app_props.xml -include:folder client /vdir permanent
 
-#Cleanup
+echo Cleanup
 
 rm ./calculator.tnpak
 rm ./currency.tnpak
--- demo1/make_sample_x64.cmd
+++ demo1/make_sample_x64.cmd
@@ -10,7 +10,7 @@ rem Build component packages
 
 rem Build application package
 
-%tnaps%\tnpack demo_app.tnpak -app -author TN -name DemoApplication1 -hpage http://tnaps.tncor.com copy -ver 1.0 -desc server\appdoc.xml NONE -dep Name=hostprovider,Version=1.0,SignatureToken=0024000004800000 r hp NONE -dep Name=calculator,Version=1.0,SignatureToken=7F33AE53726B526A d calc NONE -dep Name=currency,Version=1.0,SignatureToken=7F33AE53726B526A d currency NONE -rels hp calc -rels hp currency -sign:rsa server\sign_key.rsa -include Calculator.tnpak Calculator.tnpak public -include Currency.tnpak Currency.tnpak public -props server\app_props.xml -include:folder client /vdir permanent
+%tnaps%\tnpack demo_app.tnpak -app -author TN -name DemoApplication1 -hpage http://tnaps.tncor.com copy -ver 1.0 -desc server\appdoc.xml server\app.png -dep Name=hostprovider,Version=1.0,SignatureToken=0024000004800000 r hp NONE -dep Name=calculator,Version=1.0,SignatureToken=7F33AE53726B526A d calc NONE -dep Name=currency,Version=1.0,SignatureToken=7F33AE53726B526A d currency NONE -rels hp calc -rels hp currency -sign:rsa server\sign_key.rsa -include Calculator.tnpak Calculator.tnpak public -include Currency.tnpak Currency.tnpak public -props server\app_props.xml -include:folder client /vdir permanent
 
 rem Cleanup
 
